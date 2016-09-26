9c4b0b82f350c2c3dc8a3bbb7b469292a649eff8
haofung <haofung1221@hotmail.com>
2016-09-26 16:14:32 +0800

    done storyboard waiting for login

---
+++ Traveller/PostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewController.swift
+//  Traveller
+//
+//  Created by Hahaboy on 26/09/2016.
+//  Copyright © 2016 Tan Yee Gene. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
