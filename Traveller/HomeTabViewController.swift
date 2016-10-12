2ca7228a2c28f04093ed4fad565b59ef1bb993dc
haofung <haofung1221@hotmail.com>
2016-10-12 12:41:12 +0800

    final, fix home structure

--- Traveller/HomeTabViewController.swift
+++ Traveller/HomeTabViewController.swift
@@ -104,10 +104,11 @@ class HomeTabViewController: UIViewController, UICollectionViewDelegate, UIColle
         
         cell.layer.cornerRadius = cell.frame.width / 30
         
-        cell.priceLabel.backgroundColor = StyleKit.skinColor
-        cell.productNameLabel.backgroundColor = StyleKit.skinColor
+        cell.backgroundColor = StyleKit.skinColor
+//        cell.productNameLabel.backgroundColor = StyleKit.skinColor
         cell.productNameLabel.attributedText = self.BoldString(post.productName,size: 15)
-        cell.priceLabel.text = "RM " + post.price
+        let tempString = "RM " + post.price
+        cell.priceLabel.attributedText = self.BoldString(tempString,size: 11)
         
         cell.layer.shadowColor = UIColor.blackColor().CGColor
         cell.layer.shadowOffset = CGSizeMake(0, 1)
